156ac45f180ba0f12213bcb2d8329773d46db647
Brandon <bduong30@ufl.edu>
2016-03-31 18:19:13 -0400

    models

---
+++ src/com/wecanteven/Models/Entities/AvatarState.java
@@ -0,0 +1,21 @@
+package com.wecanteven.Models.Entities;
+
+import com.wecanteven.UtilityClasses.Direction;
+
+/**
+ * Created by Brandon on 3/31/2016.
+ */
+public abstract class AvatarState {
+    abstract void attack(Direction d);
+    abstract void useAbility(int index);
+    abstract boolean equipItem(String id);
+    abstract boolean unequipItem(String id);
+    abstract boolean equipAbility(String id);
+    abstract boolean unequipAbility(String id);
+    abstract boolean consume(String id);
+    abstract void drop();
+    abstract void pickup();
+    abstract void interactWith();
+    abstract void mount();
+    abstract void dismount();
+}
---
+++ src/com/wecanteven/Models/Entities/NPC.java
@@ -0,0 +1,8 @@
+package com.wecanteven.Models.Entities;
+
+/**
+ * Created by Brandon on 3/31/2016.
+ */
+public class NPC extends Character {
+    private void interact(){}
+}
