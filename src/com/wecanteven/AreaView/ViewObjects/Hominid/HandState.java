0ea59e962fd659addb790800f862b708b94be77d
Ian Cavitt <icavitt18@gmail.com>
2016-03-31 20:01:04 -0400

    Adam--- starting Hands in AreaView

---
+++ src/com/wecanteven/AreaView/ViewObjects/Hominid/HandState.java
@@ -0,0 +1,45 @@
+package com.wecanteven.AreaView.ViewObjects.Hominid;
+
+
+import com.wecanteven.UtilityClasses.Direction;
+import com.wecanteven.UtilityClasses.Location;
+
+import java.awt.*;
+
+public abstract class HandState {
+    public void drawForeground(Graphics2D graphic) {
+        //TODO
+    }
+
+    public void drawBackground(Graphics2D graphic) {
+        //TODO
+    }
+
+    public void draw(Graphics2D graphic) {
+        //TODO
+    }
+
+    public void move(Graphics2D graphic) {
+        //TODO
+    }
+
+    public void changeDirection(Direction direction) {
+        //TODO
+    }
+
+    public void setLocation(Location location) {
+        //TODO
+    }
+
+    public Location getLocation() {
+        return null;
+    }
+
+    public void attack(long durationOfAttack) {
+        //TODO
+    }
+
+    public void equip(/*add weapon param model doesnt exist*/) {
+
+    }
+}
