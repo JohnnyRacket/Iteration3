6c243e3fa99fde9c1f61479a78eb39bd49bfd3d2
Brandon <bduong30@ufl.edu>
2016-03-31 20:11:16 -0400

    stats

---
+++ src/com/wecanteven/Models/Entities/EntityState.java
@@ -0,0 +1,31 @@
+package com.wecanteven.Models.Entities;
+
+import com.wecanteven.UtilityClasses.Direction;
+
+/**
+ * Created by Brandon on 3/31/2016.
+ */
+public class EntityState {
+    public void attack(Direction d){}
+    public void useAbility(int index){}
+    public boolean equipItem(String id){
+        return false;
+    }
+    public boolean unequipItem(String id){
+        return false;
+    }
+    public boolean equipAbility(String id){
+        return false;
+    }
+    public boolean unequipAbility(String id){
+        return false;
+    }
+    public boolean consume(String id){
+        return false;
+    }
+    public void drop(){}
+    public void pickup(){}
+    public void interactWith(){}
+    public void mount(){}
+    public void dismount(){}
+}
