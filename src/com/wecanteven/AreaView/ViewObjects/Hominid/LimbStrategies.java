ae8758123a4cb517631fcd90b92694a714b077c5
alexs10 <alexs10@ufl.edu>
2016-04-06 19:55:16 -0400

    limb strategy

--- src/com/wecanteven/AreaView/ViewObjects/Hominid/LimbStrategies.java
+++ src/com/wecanteven/AreaView/ViewObjects/Hominid/LimbStrategies.java
@@ -7,10 +7,25 @@ import com.wecanteven.AreaView.ViewTime;
  */
 public abstract class LimbStrategies {
 
+    public abstract void animate(double percentage);
+
     public void execute(long duration) {
         long startTime = ViewTime.getInstance().getCurrentTime();
 
     }
 
-//    private double calculatePercentage(long startTime, long endTime, lon
+    private void step(long startTime, long duration) {
+        double percentage = calculatePercentage(startTime, duration);
+        if (percentage < 1) {
+            animate(percentage);
+            ViewTime.getInstance().register(() -> step(startTime, duration), 1);
+        }
+
+    }
+
+    private double calculatePercentage(long startTime, long duration) {
+        long t = ViewTime.getInstance().getCurrentTime();
+        double percentage = (double)(t-startTime)/duration;
+        return percentage < 1 ? percentage : 1;
+    }
 }
