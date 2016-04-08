e047f180382069e9aa62661b52df3aeda3726f92
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-04-07 23:38:08 -0400

    fully working title bar Decoratorgit add .git add .

--- src/com/wecanteven/MenuView/DrawableContainers/Decorators/TitleBarDecorator.java
+++ src/com/wecanteven/MenuView/DrawableContainers/Decorators/TitleBarDecorator.java
@@ -17,7 +17,8 @@ public class TitleBarDecorator extends DrawableDecorator {
     public TitleBarDecorator(Drawable d, String title) {
         super(d);
         this.title = title;
-        d.setHeight(d.getHeight()-50);
+        this.setHeight(d.getHeight());
+        //d.setHeight(d.getHeight()-50);
     }
 
     @Override
@@ -28,13 +29,18 @@ public class TitleBarDecorator extends DrawableDecorator {
         g2d.setFont(new Font("Helvetica",1,20));
         FontMetrics metrics = g2d.getFontMetrics();
         g2d.drawString(title,x + this.getDrawable().getWidth()/2 - metrics.stringWidth(title)/2,y+30);
+        //this.getDrawable().setHeight(this.getHeight()-50);
         super.draw(g2d,x,y+50, windowWidth, windowHeight);
     }
 
     @Override
     public void setHeight(int height) {
-        super.setHeight(height);
-        this.getDrawable().setHeight(height-50);
+        //System.out.println
+        super.setHeight(height - 50);
+    }
+    @Override
+    public int getHeight(){
+        return super.getHeight()+50;
     }
 
     public Color getBgColor() {
