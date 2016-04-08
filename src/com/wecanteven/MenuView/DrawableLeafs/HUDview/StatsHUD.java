fdefb7880aa987ef9d13408795d2e943c850ef83
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-04-08 04:37:04 -0400

    added smooth smooth anti aliasing

--- src/com/wecanteven/MenuView/DrawableLeafs/HUDview/StatsHUD.java
+++ src/com/wecanteven/MenuView/DrawableLeafs/HUDview/StatsHUD.java
@@ -5,7 +5,10 @@ import com.wecanteven.MenuView.DrawableLeafs.ProgressBars.AnimatedChangeProgress
 import com.wecanteven.Models.Stats.*;
 import com.wecanteven.Observers.Observer;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * Created by John on 4/7/2016.
@@ -18,26 +21,36 @@ public class StatsHUD extends Drawable implements Observer {
     private Color bgColor = Color.GRAY;
     private CircularHealthBar healthBar, manaBar;
     private  AnimatedChangeProgressBar expBar;
+    private BufferedImage stoneTexture;
+    private TexturePaint texture;
+    private Color borderColor = new Color(40,40,40);
 
     public StatsHUD(Stats stats){
         this.stats = stats;
         healthBar = new CircularHealthBar(180,180);
         healthBar.setCurrentColor(new Color(40,250,80));
         healthBar.setDepletedColor(new Color(20,140,40));
-        healthBar.setBorderSize(20);
-        healthBar.setBorderColor(new Color(80,80,80));
+        healthBar.setBorderSize(14);
+        healthBar.setBorderColor(borderColor);
         //healthBar.setBorderWidth(8);
         manaBar = new CircularHealthBar(180,180);
         manaBar.setCurrentColor(new Color(10,120,255));
         manaBar.setDepletedColor(new Color(10,70,175));
-        manaBar.setBorderSize(20);
-        manaBar.setBorderColor(new Color(80,80,80));
+        manaBar.setBorderSize(14);
+        manaBar.setBorderColor(borderColor);
 
         expBar = new AnimatedChangeProgressBar(200,50);
         expBar.setCurrentColor(new Color(230,170,50));
         expBar.setDepletedColor(new Color(150,100,25));
-        expBar.setBorderWidth(20);
-        expBar.setBorderColor(new Color(80,80,80));
+        expBar.setBorderWidth(14);
+        expBar.setBorderColor(borderColor);
+//        try {
+
+//            stoneTexture = ImageIO.read(new File("resources/textures/stone.png"));
+//        }catch(Exception e){
+//            System.out.println("failed to load stone texture");
+//        }
+//        texture = new TexturePaint(stoneTexture, new Rectangle(0, 0, 90, 60));
 
         update();
     }
@@ -61,19 +74,30 @@ public class StatsHUD extends Drawable implements Observer {
         expBar.draw(g2d,240,0,windowWidth,windowHeight);
 
 
-        g2d.setColor(new Color(80,80,80));
+        g2d.setColor(borderColor);
         g2d.fillPolygon(new int[]{0,0,300},new int[]{0,300,0},3);
 
         g2d.fillPolygon(new int[]{windowWidth,windowWidth,windowWidth - 300},new int[]{0,300,0},3);
 
-        g2d.setColor(new Color(95,95,95));
-        g2d.fillPolygon(new int[]{0,0,280},new int[]{0,280,0},3);
-        g2d.fillPolygon(new int[]{windowWidth,windowWidth,windowWidth - 280},new int[]{0,280,0},3);
+
+        //g2d.setPaint(texture);
+        g2d.setColor(new Color(80,80,80));
+        g2d.fillPolygon(new int[]{0,0,290},new int[]{0,290,0},3);
+        g2d.fillPolygon(new int[]{windowWidth,windowWidth,windowWidth - 290},new int[]{0,290,0},3);
 
 
         healthBar.draw(g2d,20,20,windowWidth,windowHeight);
 
         manaBar.draw(g2d,windowWidth - manaBar.getWidth() - 20,20,windowWidth,windowHeight);
+
+        RenderingHints rhreset = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_DEFAULT);
+
+        rhreset.put(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_DEFAULT);
+
+        g2d.setRenderingHints(rhreset);
 
     }
 
