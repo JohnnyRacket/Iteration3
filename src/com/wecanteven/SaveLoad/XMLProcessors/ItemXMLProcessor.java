cee7dbcf0f2f357bb48e2931ba58daf3bbc22c60
Joshua Kegley <josh1289op@gmail.com>
2016-04-09 18:18:37 -0400

    Added loading NPCs, All of the obstacles, oneshot, and interactive items from save file

--- src/com/wecanteven/SaveLoad/XMLProcessors/ItemXMLProcessor.java
+++ src/com/wecanteven/SaveLoad/XMLProcessors/ItemXMLProcessor.java
@@ -1,6 +1,12 @@
 package com.wecanteven.SaveLoad.XMLProcessors;
 
+import com.wecanteven.Models.Factories.ItemFactories.InteractiveItemFactory;
+import com.wecanteven.Models.Factories.ItemFactories.ObstacleItemFactory;
+import com.wecanteven.Models.Factories.ItemFactories.OneShotItemFactory;
+import com.wecanteven.Models.Items.InteractiveItem;
 import com.wecanteven.Models.Items.Item;
+import com.wecanteven.Models.Items.Obstacle;
+import com.wecanteven.Models.Items.OneShot;
 import com.wecanteven.Models.Items.Takeable.TakeableItem;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
@@ -16,11 +22,21 @@ public class ItemXMLProcessor extends XMLProcessor {
         ArrayList<Attr> attr = new ArrayList<>();
         attr.add(sf.saveAttr("name", i.getName()));
         System.out.println("Adding item to most recent: " + type + ":" + i.getName());
-        sf.appendObjectToMostRecent(sf.createSaveElement("Item", attr));
+        sf.appendObjectToMostRecent(sf.createSaveElement(type, attr));
     }
 
     public static TakeableItem parseTakeableItem(Element el) {
         return null;
     }
+    public static Obstacle parseObstacleItem(Element el) {
+        return new ObstacleItemFactory().vendObstacleItem(el.getAttribute("name"));
+    }
+    public static InteractiveItem parseInteractiveItem(Element el) {
+        return new InteractiveItem(el.getAttribute("name"));
+    }
+    public static OneShot parseOneShotItem(Element el) {
+        return new OneShotItemFactory().vendDefaultOneShot(el.getAttribute("name"));
+    }
+
 
 }
