6f40c78162b77ffb264f44b10fb1fa775ed72924
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-04-11 19:11:37 -0400

    working on changeable keybindings

---
+++ src/com/wecanteven/Controllers/InputControllers/ControllerStates/KeyBindState.java
@@ -0,0 +1,19 @@
+package com.wecanteven.Controllers.InputControllers.ControllerStates;
+
+import com.wecanteven.Controllers.InputControllers.MainController;
+
+import javax.swing.*;
+
+/**
+ * Created by John on 4/11/2016.
+ */
+public class KeyBindState extends ControllerState {
+    public KeyBindState(JFrame jFrame, MainController controller) {
+        super(jFrame, controller);
+    }
+
+    @Override
+    public void createKeybindings() {
+
+    }
+}
--- src/com/wecanteven/MenuView/UIViewFactory.java
+++ src/com/wecanteven/MenuView/UIViewFactory.java
@@ -13,6 +13,7 @@ import com.wecanteven.MenuView.DrawableContainers.Decorators.VerticalCenterConta
 import com.wecanteven.MenuView.DrawableContainers.LayoutComposites.ColumnatedCompositeContainer;
 import com.wecanteven.MenuView.DrawableContainers.LayoutComposites.CustomScaleColumnsContainer;
 import com.wecanteven.MenuView.DrawableLeafs.HUDview.StatsHUD;
+import com.wecanteven.MenuView.DrawableLeafs.KeyBindView;
 import com.wecanteven.MenuView.DrawableLeafs.NavigatableGrids.GridItem;
 import com.wecanteven.MenuView.DrawableLeafs.NavigatableGrids.NavigatableGrid;
 import com.wecanteven.MenuView.DrawableLeafs.ScrollableMenus.*;
@@ -431,6 +432,12 @@ public class UIViewFactory {
             java.util.Map.Entry pair = (java.util.Map.Entry)it.next();
             list.addItem(new ScrollableMenuItem(pair.getKey() + " ---> " + pair.getValue(), ()->{
                 //do something
+                SwappableView view = new SwappableView();
+                view.addDrawable(new VerticalCenterContainer(new HorizontalCenterContainer(new KeyBindView(40,200))));
+                ViewTime.getInstance().register(()->{
+                    vEngine.getManager().addView(view);
+                },0);
+                //controller.setMenuState(view.getMenuViewContainer());
 
             }));
         }
