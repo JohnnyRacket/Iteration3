bc42738854c0bf4524e86039eb660397465c3308
audgster <ahs3028@gmail.com>
2016-04-14 20:40:35 -0400

    Updated Aoe to tick every 30 ticks

--- src/com/wecanteven/Models/Map/Aoe/TickableAreaOfEffect.java
+++ src/com/wecanteven/Models/Map/Aoe/TickableAreaOfEffect.java
@@ -2,6 +2,7 @@ package com.wecanteven.Models.Map.Aoe;
 
 import com.wecanteven.Models.Entities.Entity;
 import com.wecanteven.Models.Map.Tile;
+import com.wecanteven.Models.ModelTime.Alertable;
 import com.wecanteven.Models.ModelTime.ModelTime;
 import com.wecanteven.Models.ModelTime.Tickable;
 import com.wecanteven.Visitors.AreaOfEffectVisitor;
@@ -9,19 +10,23 @@ import com.wecanteven.Visitors.AreaOfEffectVisitor;
 /**
  * Created by simonnea on 4/11/16.
  */
-public abstract class TickableAreaOfEffect extends AreaOfEffect implements Tickable {
+public abstract class TickableAreaOfEffect extends AreaOfEffect implements Tickable, Alertable {
     private Entity entity;
     private Tile.TileSlot<Entity> observee;
+    private boolean active;
 
     public TickableAreaOfEffect() {
         ModelTime.getInstance().registerTickable(this);
+        active = true;
     }
 
     @Override
     public void tick() {
-        if (entity != null) {
+        if (entity != null && active) {
             System.out.println("Tick apply");
             apply(entity);
+            active = false;
+            ModelTime.getInstance().registerAlertable(this, 30);
         }
     }
 
@@ -46,5 +51,10 @@ public abstract class TickableAreaOfEffect extends AreaOfEffect implements Ticka
     @Override
     public void accept(AreaOfEffectVisitor visitor) {
         visitor.visitTickableAoe(this);
+    }
+
+    @Override
+    public void alert() {
+        active = true;
     }
 }
