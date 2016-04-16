65c794ba652e95c36bd5b42581ca217f2ca39dce
FiveTierAdam <fortieradam@gmail.com>
2016-04-16 18:27:17 -0400

    Added a fix to mount state

--- src/com/wecanteven/Models/Entities/AvatarStates/MountState.java
+++ src/com/wecanteven/Models/Entities/AvatarStates/MountState.java
@@ -1,8 +1,10 @@
 package com.wecanteven.Models.Entities.AvatarStates;
 
+import com.wecanteven.AreaView.Position;
 import com.wecanteven.Models.Entities.Character;
 import com.wecanteven.Models.Entities.Mount;
 import com.wecanteven.UtilityClasses.Direction;
+import com.wecanteven.UtilityClasses.Location;
 
 /**
  * Created by Brandon on 3/31/2016.
@@ -18,9 +20,20 @@ public class MountState extends AvatarState {
     }
     public boolean move(Direction d){
         boolean moved = true;
-        //moved = avatar.move(d) && moved;
         moved = mount.move(d) && moved;
+
+        avatar.move(mount.getLocation());
+        //avatar.setLocation(mount.getLocation());
+        //updatePlayerLocation();
+        System.out.println("Avatar Location:" + avatar.getLocation());
         return moved;
+    }
+
+    private void updatePlayerLocation() {
+        Location avatarLocation = avatar.getLocation();
+        avatarLocation.setR(mount.getLocation().getR());
+        avatarLocation.setS(mount.getLocation().getS());
+        avatarLocation.setZ(mount.getLocation().getZ());
     }
     public void attack(){}
     public void useAbility(int index){}
