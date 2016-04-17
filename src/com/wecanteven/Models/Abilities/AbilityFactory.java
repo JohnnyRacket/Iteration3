9a7c15e5b8b10fad41500eeb1f9790578f10b8b7
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-04-17 00:55:15 -0400

    changed tile interaction

--- src/com/wecanteven/Models/Abilities/AbilityFactory.java
+++ src/com/wecanteven/Models/Abilities/AbilityFactory.java
@@ -17,19 +17,19 @@ public class AbilityFactory {
         MeleeRangeHitBoxGenerator generator = new MeleeRangeHitBoxGenerator("Punch",caster,effect);
         return new Ability("Punch",caster,generator);
     }
-    public Ability vendRangedAttack(Character caster) {
-        StatsEffect effect = new StatsEffect(new StatsAddable(0,0,0,0,0,0,0,-1,0));
-        ProjectileHitBoxGenerator generator = new ProjectileHitBoxGenerator(caster,effect);
-        return new Ability(caster,generator);
-    }
-    public Ability vendRadialAttack(Character caster) {
-        StatsEffect effect = new StatsEffect(new StatsAddable(0,0,0,0,0,0,0,-1,0));
-        RadialHitBoxGenerator generator = new RadialHitBoxGenerator(caster,effect);
-        return new Ability(caster,generator);
-    }
-    public Ability vendDomeAttack(Character caster) {
-        StatsEffect effect = new StatsEffect(new StatsAddable(0,0,0,0,0,0,0,-1,0));
-        DomeHitBoxGenerator generator = new DomeHitBoxGenerator(caster,effect);
-        return new Ability(caster,generator);
-    }
+//    public Ability vendRangedAttack(Character caster) {
+//        StatsEffect effect = new StatsEffect(new StatsAddable(0,0,0,0,0,0,0,-1,0));
+//        ProjectileHitBoxGenerator generator = new ProjectileHitBoxGenerator(caster,effect);
+//        return new Ability(caster,generator);
+//    }
+//    public Ability vendRadialAttack(Character caster) {
+//        StatsEffect effect = new StatsEffect(new StatsAddable(0,0,0,0,0,0,0,-1,0));
+//        RadialHitBoxGenerator generator = new RadialHitBoxGenerator(caster,effect);
+//        return new Ability(caster,generator);
+//    }
+//    public Ability vendDomeAttack(Character caster) {
+//        StatsEffect effect = new StatsEffect(new StatsAddable(0,0,0,0,0,0,0,-1,0));
+//        DomeHitBoxGenerator generator = new DomeHitBoxGenerator(caster,effect);
+//        return new Ability(caster,generator);
+//    }
 }
