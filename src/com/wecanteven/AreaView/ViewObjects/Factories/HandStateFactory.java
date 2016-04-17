3bb56a8427648da78ce1aa68a6b7aa9aa882fd0f
Alex Smith <alexs10@ufl.edu>
2016-04-17 12:48:58 -0400

    more visuals

---
+++ src/com/wecanteven/AreaView/ViewObjects/Factories/HandStateFactory.java
@@ -0,0 +1,10 @@
+package com.wecanteven.AreaView.ViewObjects.Factories;
+
+import com.wecanteven.AreaView.ViewObjects.Hominid.Hands.HandState;
+
+/**
+ * Created by alexs on 4/17/2016.
+ */
+public class HandStateFactory {
+    //public HandState create
+}
