4c07463b391c6ae1648ddd6522ec6cc046f328fb
audgster <ahs3028@gmail.com>
2016-04-17 17:17:27 -0400

    Starting ability storage

---
+++ src/com/wecanteven/Models/Storage/AbilityStorage/InfiniteAbilityInventory.java
@@ -0,0 +1,48 @@
+package com.wecanteven.Models.Storage.AbilityStorage;
+
+import com.wecanteven.Models.Abilities.Ability;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by simonnea on 4/17/16.
+ */
+public class InfiniteAbilityInventory extends AbilityInventory {
+    private ArrayList<Ability> abilities;
+
+    public InfiniteAbilityInventory(AbilityStorage owner) {
+        super(owner);
+        this.abilities = new ArrayList<>();
+    }
+
+    @Override
+    public boolean addAbility(Ability ability) {
+        abilities.add(ability);
+        return true;
+    }
+
+    @Override
+    public boolean removeAbilty(Ability ability) {
+        if (containsAbility(ability)) {
+            abilities.remove(ability);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAbility(Ability ability) {
+        return abilities.contains(ability);
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public Iterator<Ability> getIterator() {
+        return abilities.iterator();
+    }
+}
